3875f09159ce59badf846b428eba70df3c92b037
Marco Scheel <ich+git@marcoscheel.de>
2016-02-05 16:35:17 +0100

    npm install applicationinights start app insights if the instrumentation key is configured in azure app settings

--- index.js
+++ index.js
@@ -13,6 +13,18 @@ express = require('express');
 ghost = require('./core');
 errors = require('./core/server/errors');
 
+// Azure App Insights
+// ------------------------------------------------------------------------
+// If the App Setting 'websiteUrl' is set, Ghost will use that URL as base.
+// If it isn't set, we'll go with the default sitename.
+var instrumentationKey = process.env.instrumentationKey;
+if (!instrumentationKey || instrumentationKey === '' || instrumentationKey.length === 0) {
+	appInsights = require("applicationinsights");
+	console.log(instrumentationKey);
+	appInsights.setup(instrumentationKey).start();
+}
+
+
 // Create our parent express app instance.
 parentApp = express();
 
@@ -22,7 +34,8 @@ ghost().then(function (ghostServer) {
     parentApp.use(ghostServer.config.paths.subdir, ghostServer.rootApp);
 
     // Let Ghost handle starting our server instance.
-    ghostServer.start(parentApp);
+	ghostServer.start(parentApp);
+
 }).catch(function (err) {
     errors.logErrorAndExit(err, err.context, err.help);
 });
